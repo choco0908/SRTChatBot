72ddd4a7b80d38db8ef9b242647df2f659501553
HGKwon_Mac_CO <starfish0114@gmail.com>
2020-04-29 13:43:48 +0900

    예약진행 버튼식으로 변경

--- srtchat.py
+++ srtchat.py
@@ -1,14 +1,16 @@
 import time
 import telepot
 from telepot.loop import MessageLoop
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 import os
 import re
 from SRT import SRT
 
-checkIDMsg = "예약을 진행하겠습니다.\n" \
-             "1. 예약\n" \
-             "4. 종료"
-
+checkIDMsg = "예약을 진행하겠습니다."
+keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                   [InlineKeyboardButton(text='1. 예약', callback_data='1')],
+                   [InlineKeyboardButton(text='4. 종료', callback_data='4')],
+               ])
 pattern = "[a-zA-Z0-9]+[/][a-zA-Z0-9!@#$%^&*()]+"
 
 def getToken():
@@ -22,7 +24,7 @@ def handle(msg):
     content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(msg, long=True)
     print(content_type,chat_type,chat_id,msg_date,msg_id)
     print(msg)
-    print('[ --------- Message Received --------- ]')
+    print('[ --------- Message Received --------- ]\n\n')
 
     if content_type == 'text':
         if msg['text'] == '1':
@@ -34,7 +36,7 @@ def handle(msg):
             bot.sendMessage(chat_id,'종료')
             os._exit(1)
         else:
-            bot.sendMessage(chat_id,checkIDMsg)
+            bot.sendMessage(chat_id,checkIDMsg,reply_markup=keyboard)
 
 
 if __name__ == '__main__':
@@ -44,6 +46,7 @@ if __name__ == '__main__':
     MessageLoop(bot,handle).run_forever()
     #bot.message_loop(handle,run_forever=True)
     print('[+] ---------- Start Listening ----------')
+
 
     while True:
         time.sleep(10)
