5fdeeb1a656e194e4860ae790284bab8e9f51873
HGKwon_Mac_CO <starfish0114@gmail.com>
2020-05-07 14:09:08 +0900

    로그인, 예약, 조회, 취소 기능구현 완료

--- srtchat.py
+++ srtchat.py
@@ -10,16 +10,22 @@ import json
 import os
 import re
 from SRT import SRT
+#from passengers import Adult, Child
 
 users = {}
 
-login_pattern = "[a-zA-Z0-9]+[/][a-zA-Z0-9!@#$%^&*()]+"
+login_pattern = "[a-zA-Z0-9]+[/].+"
 reserve_pattern = "[\u3131-\u3163\uac00-\ud7a3]+[/][\u3131-\u3163\uac00-\ud7a3]+[/][0-9]+[/][0-9]+"
+person_pattern = "[0-5]+[/]+[0-5]"
+time_pattern = re.compile(r"[\u3131-\u3163\uac00-\ud7a3]+[~][\u3131-\u3163\uac00-\ud7a3]+[(][0-9:~]+[)]")
+seat_pattern = re.compile(r"[\u3131-\u3163\uac00-\ud7a3]+[\s][\u3131-\u3163\uac00-\ud7a3]+")
 #email_pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
 #number_pattern = re.compile(r"(\d{3})-(\d{3,4})-(\d{4})")
 startMsg = "아직은 기능 개발중입니다.\n지금은 일반실만 예약가능합니다.\n예약을 진행하겠습니다."
 startKeyboard = InlineKeyboardMarkup(inline_keyboard=[
                    [InlineKeyboardButton(text='1. 예약', callback_data='reserve')],
+                   [InlineKeyboardButton(text='2. 조회', callback_data='refers')],
+                   [InlineKeyboardButton(text='3. 취소', callback_data='cancel')],
                    [InlineKeyboardButton(text='4. 종료', callback_data='exit')],
                ])
 
@@ -33,12 +39,48 @@ def getToken():
 def reserve_message(chat_id,trains):
     keyboard =[]
     i = 0
-    for train in trains:
-        key = []
-        key.append(InlineKeyboardButton(text=str(train),callback_data=str(i)))
-        keyboard.append(key)
-        i = i+1
-    keyboard.append([InlineKeyboardButton(text='예약 종료', callback_data='exit')])
+    if len(trains) == 0:
+        keyboard.append([InlineKeyboardButton(text='해당 시간에는 기차가 없습니다.', callback_data='')])
+    else:
+        for train in trains:
+            key = []
+            time = time_pattern.findall(str(train))[0]
+            seat = seat_pattern.findall(str(train))[1]
+            key.append(InlineKeyboardButton(text=time+' '+seat,callback_data='reserve_'+str(i)))
+            keyboard.append(key)
+            i = i+1
+    keyboard.append([InlineKeyboardButton(text='예약 종료', callback_data='back')])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard)
+    bot.sendMessage(chat_id,'조회 완료', reply_markup=keyboard)
+
+def refer_message(chat_id,trains):
+    print(trains)
+    keyboard =[]
+    i = 0
+    if len(trains) == 0:
+        keyboard.append([InlineKeyboardButton(text='예약된 기차가 없습니다.', callback_data='')])
+    else:
+        for train in trains:
+            key = []
+            key.append(InlineKeyboardButton(text=str(train),callback_data='refer_'+str(i)))
+            keyboard.append(key)
+            i = i+1
+    keyboard.append([InlineKeyboardButton(text='조회 종료', callback_data='back')])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard)
+    bot.sendMessage(chat_id,'조회 완료', reply_markup=keyboard)
+
+def cancel_message(chat_id,trains):
+    keyboard =[]
+    i = 0
+    if len(trains) == 0:
+        keyboard.append([InlineKeyboardButton(text='예약된 기차가 없습니다.', callback_data='')])
+    else:
+        for train in trains:
+            key = []
+            key.append(InlineKeyboardButton(text=str(train),callback_data='cancel_'+str(i)))
+            keyboard.append(key)
+            i = i+1
+    keyboard.append([InlineKeyboardButton(text='취소 종료', callback_data='back')])
     keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard)
     bot.sendMessage(chat_id,'조회 완료', reply_markup=keyboard)
 
@@ -57,14 +99,15 @@ def handle_message(msg):
                 srt = SRT(user_id,user_pwd,verbose=True)
                 users[chat_id] = {}
                 users[chat_id]['srt'] = srt
+                print(chat_id,' User logged in')
                 bot.sendMessage(chat_id,'{} 로그인 완료'.format(user_id))
                 bot.sendMessage(chat_id,'조회할 기차 정보를 입력하세요(출발지/도착지/날짜/시간)\nex)수서/부산/20190913/144000')
+                #bot.sendMessage(chat_id,'인원수를 입력하세요.\n입력이 없으면 어른1명으로 예약됩니다.\nex)3/1')
             except Exception as e:
                 print(e)
                 bot.sendMessage(chat_id,'로그인 실패\n계정정보를 입력하세요(USER_ID/USER_PW)\nex)id_1234/pw_1234')
         elif re.match(reserve_pattern,msg['text']):
             if chat_id in users:
-                print(chat_id,' User logged in')
                 srt = users.get(chat_id).get('srt')
                 infos = msg['text'].split('/')
                 dep = infos[0]
@@ -77,11 +120,36 @@ def handle_message(msg):
                     reserve_message(chat_id,trains)
                 except Exception as e:
                     print(e)
-                    bot.sendMessage(chat_id,'조희 실패\n조회할 기차 정보를 입력하세요(출발지/도착지/날짜/시간)\nex)수서/부산/20190913/144000')
+                    bot.sendMessage(chat_id,'조회 실패\n조회할 기차 정보를 입력하세요(출발지/도착지/날짜/시간)\nex)수서/부산/20190913/144000')
             else:
                 bot.sendMessage(chat_id,'로그인 정보가 없습니다\n계정정보를 입력하세요(USER_ID/USER_PW)\nex)id_1234/pw_1234')
         else:
             bot.sendMessage(chat_id,startMsg, reply_markup=startKeyboard)
+        '''
+        elif re.match(person_pattern,msg['text']):
+            if chat_id in users:
+                srt = users.get(chat_id).get('srt')
+                persons = msg['text'].split('/')
+                adult = int(persons[0])
+                child = int(persons[1])
+                if (adult+child) > 9 or (adult+child) == 0 :
+                    bot.sendMessage(chat_id,'인원수는 최소 1명 최대 9명까지 가능합니다.\n입력이 없으면 어른1명으로 예약됩니다.')
+                    return
+                passenger = []
+                for i in range(adult):
+                    passenger.append(Adult())
+                for i in range(child):
+                    passenger.append(Child())
+                try:
+                    users[chat_id]['passenger'] = passenger
+                except Exception as e:
+                    print(e)
+                    bot.sendMessage(chat_id,'인원수는 최소 1명 최대 9명까지 가능합니다.\n입력이 없으면 어른1명으로 예약됩니다.')
+            else:
+                bot.sendMessage(chat_id,'로그인 정보가 없습니다\n계정정보를 입력하세요(USER_ID/USER_PW)\nex)id_1234/pw_1234')
+        else:
+            bot.sendMessage(chat_id,startMsg, reply_markup=startKeyboard)
+        '''
 
 def reserve_query(msg): 
     global users
@@ -89,11 +157,60 @@ def reserve_query(msg):
     print('Callback Query:', query_id, from_id, query_data)
     if query_data == 'reserve':
         bot.sendMessage(from_id,'계정정보를 입력하세요(USER_ID/USER_PW)\nex)id_1234/pw_1234')
+    elif 'reserve_' in query_data:
+        print(query_data+'예약진행')
+        num = int(query_data.split('_')[1])
+        try:
+            srt = users.get(from_id).get('srt')
+            train = users.get(from_id).get('trains')[num]        
+            #if 'passenger' in users.get(from_id):
+            #    passenger = users.get(from_id).get('passenger')
+            #    srt.reserve(train,passengers=passenger)
+            #else:
+            
+            reservation = srt.reserve(train)
+            bot.sendMessage(from_id,str(reservation)+'예약완료')
+            bot.sendMessage(from_id,startMsg, reply_markup=startKeyboard)
+        except Exception as e:
+            print(e)
+            bot.sendMessage(from_id,'예약 실패')
+    elif query_data == 'refers':
+        try:
+            srt = users.get(from_id).get('srt')
+            trains = srt.get_reservations()
+            refer_message(from_id,trains)
+        except Exception as e:
+            print(e)
+            bot.sendMessage(from_id,'조회 실패')
+    elif query_data == 'cancel':
+        try:
+            srt = users.get(from_id).get('srt')
+            trains = srt.get_reservations()
+            cancel_message(from_id,trains)
+        except Exception as e:
+            print(e)
+            bot.sendMessage(from_id,'조회 실패')
+    elif 'cancel_' in query_data:
+        print(query_data+'취소진행')
+        num = int(query_data.split('_')[1])
+        try:
+            srt = users.get(from_id).get('srt')
+            train = srt.get_reservations()[num]
+            reservation = srt.cancel(train)
+            bot.sendMessage(from_id,str(train)+'취소완료')
+            bot.sendMessage(from_id,startMsg, reply_markup=startKeyboard)
+        except Exception as e:
+            print(e)
+            bot.sendMessage(from_id,'예약 실패')
+    elif query_data == 'back':
+        bot.sendMessage(from_id,startMsg, reply_markup=startKeyboard)
     elif query_data == 'exit':
         bot.sendMessage(from_id,'종료')
         if from_id in users:
             del users[from_id]
         bot.sendMessage(from_id,startMsg, reply_markup=startKeyboard)
+        os._exit(1)
+        
 
 class MessageCounter(telepot.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
